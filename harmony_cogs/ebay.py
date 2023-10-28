ea645017db6995d02b3ab093d4f087e0755c90ee
Alvie <contact@alviemahmud.com>
2023-10-28 16:36:17 +0100

    - Update eBay base URL to allow negative keywords in search

--- harmony_cogs/ebay.py
+++ harmony_cogs/ebay.py
@@ -19,7 +19,7 @@ with open("config.json", "r") as f:
 class Ebay(commands.Cog):
 
     base_url = "https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw=$_SEARCH_QUERY" \
-               "&_in_kw=4&_ex_kw=&_sacat=0&LH_Sold=1&_udlo=&_udhi=&LH_ItemCondition=4&_samilow=&_samihi=" \
+               "&_sacat=0&LH_Sold=1&_udlo=&_udhi=&LH_ItemCondition=4&_samilow=&_samihi=" \
                "&_stpos=M300AA&_sargn=-1%26saslc%3D1&_fsradio2=%26LH_LocatedIn%3D1&_salic=3&LH_SubLocation=1" \
                "&_sop=12&_dmd=1&_ipg=60&LH_Complete=1&rt=nc&LH_PrefLoc=1"
 
